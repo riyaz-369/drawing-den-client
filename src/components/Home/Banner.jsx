6879f99e7856c13adaa9dc32c75f8face7ba350c
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-04-26 15:39:14 +0600

    banner section slider added

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -2,6 +2,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/bundle";
 import "swiper/css/pagination";
+import banner1 from "./../../assets/banner/banner1.png";
+import banner2 from "./../../assets/banner/banner2.svg";
+import banner3 from "./../../assets/banner/banner3.png";
+import banner4 from "./../../assets/banner/banner4.svg";
 import {
   Autoplay,
   Keyboard,
@@ -28,70 +32,168 @@ const Banner = () => {
         modules={[Autoplay, Navigation, Pagination, Mousewheel, Keyboard]}
       >
         <SwiperSlide>
-          <div
-            className="hero"
-            style={{
-              backgroundImage:
-                "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
-            }}
-          >
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-              <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                <p className="mb-5">
-                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                  assumenda excepturi exercitationem quasi. In deleniti eaque
-                  aut repudiandae et a id nisi.
+          <section className="dark:bg-gray-100 dark:text-gray-800">
+            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+              <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+                  Ac mattis
+                  <span className="dark:text-violet-600">senectus</span>erat
+                  pharetra
+                </h1>
+                <p className="mt-6 mb-8 text-lg sm:mb-12">
+                  Dictum aliquam porta in condimentum ac integer
+                  <br className="hidden md:inline lg:hidden" />
+                  turpis pulvinar, est scelerisque ligula sem
                 </p>
-                <button className="btn btn-primary">Get Started</button>
+                <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50"
+                  >
+                    Suspendisse
+                  </a>
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800"
+                  >
+                    Malesuada
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                <img
+                  src={banner1}
+                  alt=""
+                  className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+                />
               </div>
             </div>
-          </div>
+          </section>
         </SwiperSlide>
         <SwiperSlide>
-          <div
-            className="hero"
-            style={{
-              backgroundImage:
-                "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
-            }}
-          >
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-              <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                <p className="mb-5">
-                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                  assumenda excepturi exercitationem quasi. In deleniti eaque
-                  aut repudiandae et a id nisi.
+          <section className="dark:bg-gray-100 dark:text-gray-800">
+            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+              <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+                  Ac mattis
+                  <span className="dark:text-violet-600">senectus</span>erat
+                  pharetra
+                </h1>
+                <p className="mt-6 mb-8 text-lg sm:mb-12">
+                  Dictum aliquam porta in condimentum ac integer
+                  <br className="hidden md:inline lg:hidden" />
+                  turpis pulvinar, est scelerisque ligula sem
                 </p>
-                <button className="btn btn-primary">Get Started</button>
+                <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50"
+                  >
+                    Suspendisse
+                  </a>
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800"
+                  >
+                    Malesuada
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                <img
+                  src={banner2}
+                  alt=""
+                  className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+                />
               </div>
             </div>
-          </div>
+          </section>
         </SwiperSlide>
         <SwiperSlide>
-          <div
-            className="hero"
-            style={{
-              backgroundImage:
-                "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
-            }}
-          >
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-              <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                <p className="mb-5">
-                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                  assumenda excepturi exercitationem quasi. In deleniti eaque
-                  aut repudiandae et a id nisi.
+          <section className="dark:bg-gray-100 dark:text-gray-800">
+            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+              <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+                  Ac mattis
+                  <span className="dark:text-violet-600">senectus</span>erat
+                  pharetra
+                </h1>
+                <p className="mt-6 mb-8 text-lg sm:mb-12">
+                  Dictum aliquam porta in condimentum ac integer
+                  <br className="hidden md:inline lg:hidden" />
+                  turpis pulvinar, est scelerisque ligula sem
                 </p>
-                <button className="btn btn-primary">Get Started</button>
+                <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50"
+                  >
+                    Suspendisse
+                  </a>
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800"
+                  >
+                    Malesuada
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                <img
+                  src={banner3}
+                  alt=""
+                  className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+                />
               </div>
             </div>
-          </div>
+          </section>
+        </SwiperSlide>
+        <SwiperSlide>
+          <section className="dark:bg-gray-100 dark:text-gray-800">
+            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+              <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+                  Ac mattis
+                  <span className="dark:text-violet-600">senectus</span>erat
+                  pharetra
+                </h1>
+                <p className="mt-6 mb-8 text-lg sm:mb-12">
+                  Dictum aliquam porta in condimentum ac integer
+                  <br className="hidden md:inline lg:hidden" />
+                  turpis pulvinar, est scelerisque ligula sem
+                </p>
+                <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50"
+                  >
+                    Suspendisse
+                  </a>
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800"
+                  >
+                    Malesuada
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                <img
+                  src={banner4}
+                  alt=""
+                  className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+                />
+              </div>
+            </div>
+          </section>
         </SwiperSlide>
       </Swiper>
     </div>
