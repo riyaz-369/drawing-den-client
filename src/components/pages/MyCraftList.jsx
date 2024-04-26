d87d0f16dbbfde7ad0238d197d5857bfd059f602
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-04-26 16:01:22 +0600

    My craft list page and section content added

--- src/components/pages/MyCraftList.jsx
+++ src/components/pages/MyCraftList.jsx
@@ -1,7 +1,52 @@
 const MyCraftList = () => {
   return (
-    <div>
-      <h3 className="text-center text-3xl">Here will be added my craft list</h3>
+    <div className="max-w-7xl mx-auto my-24">
+      <div className="max-w-lg p-4 shadow-md dark:text-gray-800">
+        <div className="flex justify-between pb-4 border-bottom">
+          <div className="flex items-center">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="mb-0 capitalize dark:text-gray-800"
+            >
+              Landscape
+            </a>
+          </div>
+          <button>See All</button>
+        </div>
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <img
+              src="https://source.unsplash.com/random/480x360/?4"
+              alt=""
+              className="block object-cover object-center w-full rounded-md h-72 "
+            />
+            <div className="flex items-center text-xs">
+              <span>6 min ago</span>
+            </div>
+          </div>
+          <div className="space-y-2">
+            <a rel="noopener noreferrer" href="#" className="block">
+              <h3 className="text-xl font-semibold dark:text-violet-600">
+                Facere ipsa nulla corrupti praesentium pariatur architecto
+              </h3>
+            </a>
+            <p className="leading-snug dark:text-gray-600">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Repellat, excepturi. Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Repellat, excepturi.
+            </p>
+            <div className="flex gap-4">
+              <button className="btn bg-violet-600 text-white hover:bg-violet-500 rounded-full">
+                Update
+              </button>
+              <button className="btn bg-violet-600 text-white hover:bg-violet-500 rounded-full">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
